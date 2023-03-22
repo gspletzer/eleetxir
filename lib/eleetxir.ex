1148ecd3d0a6296a03572880c21d131f6bd92db1
Grace Spletzer <gspletzer@influxdata.com>
2023-03-21 19:30:59 -0700

    feat: Initialize project

---
+++ lib/eleetxir.ex
@@ -0,0 +1,18 @@
+defmodule Eleetxir do
+  @moduledoc """
+  Documentation for `Eleetxir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eleetxir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eleetxir_test.exs
@@ -0,0 +1,8 @@
+defmodule EleetxirTest do
+  use ExUnit.Case
+  doctest Eleetxir
+
+  test "greets the world" do
+    assert Eleetxir.hello() == :world
+  end
+end
